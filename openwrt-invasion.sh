56e2562479312c5564e8b511e0a802011636d4f1
xiv3r <117867334+xiv3r@users.noreply.github.com>
2024-10-18 17:10:34 +0800

    Update openwrt-invasion.sh

--- openwrt-invasion.sh
+++ openwrt-invasion.sh
@@ -2,8 +2,7 @@
 
 pkg install git wget python3 python-pip inetutils unzip -y
 wget https://raw.githubusercontent.com/xiv3r/termux-openwrt-invasion/refs/heads/main/openwrt-invasion.zip
-unzip openwrt-invasion.zip -d openwrt-invasion
-cd openwrt-invasion
+unzip openwrt-invasion.zip && cd openwrt-invasion
 echo "cd openwrt-invasion"
 pip install -r requirements.txt
-python3 remote_command_execution_vulnerability.py
+python3 openwrt-invasion/remote_command_execution_vulnerability.py
