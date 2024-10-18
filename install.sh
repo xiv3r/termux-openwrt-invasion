51f206318d1aa177f1dee7cd7d89d5ed3afc30fc
xiv3r <117867334+xiv3r@users.noreply.github.com>
2024-10-18 15:30:57 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,4 +8,7 @@ unzip openwrt-invasion.zip
 
 sleep 1s
 cd openwrt-invasion
-pip install -r requirements.txt && python3 remote_command_execution_vulnerability.py
+pip install -r requirements.txt
+
+sleep 1s
+python3 remote_command_execution_vulnerability.py
