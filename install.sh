72c4e2352034f7794f67e213699faeccc8baba48
xiv3r <117867334+xiv3r@users.noreply.github.com>
2024-10-18 14:59:29 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+pkg install git wget python3 python-pip inetutils unzip -y
+wget https://raw.githubusercontent.com/xiv3r/termux-openwrt-invasion/refs/heads/main/openwrt-invasion.zip
+unzip openwrt-invasion.zip && cd openwrt-invasion
+pip install -r requirements.txt && python3 remote_command_execution_vulnerability.py
