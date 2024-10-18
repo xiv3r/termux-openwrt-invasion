8ea322064d117383d7958be20563c27c9afae10e
xiv3r <117867334+xiv3r@users.noreply.github.com>
2024-10-18 15:09:20 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@
 
 pkg install git wget python3 python-pip inetutils unzip -y
 wget https://raw.githubusercontent.com/xiv3r/termux-openwrt-invasion/refs/heads/main/openwrt-invasion.zip
-unzip openwrt-invasion.zip && cd openwrt-invasion
+unzip openwrt-invasion.zip && cd && cd openwrt-invasion
 pip install -r requirements.txt
