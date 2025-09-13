2d5d227150cc8ba342cc8c8edb0ab4f8e3a0d040
rff ald <117867334+xiv3r@users.noreply.github.com>
2025-09-13 15:11:51 +0800

    Update openwrt-invasion.sh

--- openwrt-invasion.sh
+++ openwrt-invasion.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-apt install git wget python3 python-pip inetutils unzip openssh -y
-wget https://raw.githubusercontent.com/xiv3r/termux-openwrt-invasion/refs/heads/main/openwrt-invasion.zip
+pkg install git wget python3 python-pip inetutils unzip openssh -y
+wget -qO openwrt-invasion.zip https://raw.githubusercontent.com/xiv3r/termux-openwrt-invasion/refs/heads/main/openwrt-invasion.zip
 unzip openwrt-invasion.zip
 pip install -r openwrt-invasion/requirements.txt
-rm openwrt-invasion.zip
+rm -f openwrt-invasion.zip
